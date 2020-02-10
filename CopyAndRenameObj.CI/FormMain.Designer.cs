d5279f0425598a9c0a17b11a51060f0683758727
catdog50rus <catdog50rus@gmail.com>
2020-02-10 17:55:05 +0300

    Initial commit

---
+++ CopyAndRenameObj.CI/FormMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace CopyAndRenameObj.CI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textOld = new System.Windows.Forms.TextBox();
+            this.textNew = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.SelectFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Система копирования файлов с изменением части имени";
+            // 
+            // textOld
+            // 
+            this.textOld.Location = new System.Drawing.Point(223, 11);
+            this.textOld.Name = "textOld";
+            this.textOld.Size = new System.Drawing.Size(123, 20);
+            this.textOld.TabIndex = 1;
+            this.textOld.TextChanged += new System.EventHandler(this.TextOld_TextChanged);
+            // 
+            // textNew
+            // 
+            this.textNew.Location = new System.Drawing.Point(612, 11);
+            this.textNew.Name = "textNew";
+            this.textNew.Size = new System.Drawing.Size(126, 20);
+            this.textNew.TabIndex = 2;
+            this.textNew.TextChanged += new System.EventHandler(this.TextNew_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Меняемая часть имени файла / папки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(372, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "На что меняется часть имени файла / папки";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(121, 339);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(107, 30);
+            this.buttonSelect.TabIndex = 5;
+            this.buttonSelect.Text = "Выбрать папку";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.ButtonSelect_Click);
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(30, 338);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(137, 29);
+            this.buttonRun.TabIndex = 6;
+            this.buttonRun.Text = "Заменить";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.ButtonRun_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(10, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(345, 315);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSelect);
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Location = new System.Drawing.Point(3, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 383);
+            this.panel1.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.buttonCopy);
+            this.panel2.Controls.Add(this.listView2);
+            this.panel2.Controls.Add(this.buttonRun);
+            this.panel2.Location = new System.Drawing.Point(378, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(386, 383);
+            this.panel2.TabIndex = 9;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(205, 338);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(139, 30);
+            this.buttonCopy.TabIndex = 8;
+            this.buttonCopy.Text = "Скопировать";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.ButtonCopy_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(18, 13);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(345, 315);
+            this.listView2.TabIndex = 7;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.List;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.textOld);
+            this.panel3.Controls.Add(this.textNew);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(3, 30);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(761, 43);
+            this.panel3.TabIndex = 10;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 462);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textOld;
+        private System.Windows.Forms.TextBox textNew;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolderDialog;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button buttonCopy;
+    }
+}
+
